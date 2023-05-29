e2768d63a83da95af37ee9941bb88c7e146945ca
s-rajasekhar <raja_435@hotmail.com>
2023-05-29 22:01:18 +1000

    Update dictionary.c
    Tokenize input data

--- dictionary.c
+++ dictionary.c
@@ -1,40 +1,63 @@
 
 #include <stdio.h>
 
-#define FILE_PATH "./words.txt"
+#define FILE_PATH "./words.txt" /* Input File, Can be read from Console, if time permit will add. Not that important. */
 
+/* Return 1 on positive exit, else 0*/
+/*
+    Assumes, each item/element in dictonary is delimited by a new line \n
+*/
 int main()
 {
+    unsigned char *data = NULL; /*Dictonary Data will be read into this pointer*/
     FILE *fp = fopen(FILE_PATH, "r");
-    unsigned char *data = NULL;
+
     if (fp)
     {
-        unsigned long fsize = 0;
+        /* If file open success */
+        unsigned long fsize = 0; /* Data size in the file */
+        char *token;             /* Pointer for tokenization */
 
         printf("Open Dictonary Success!\n");
-        /* move to the end of the file */
+        /* Move file handle to the end of the file */
         fseek(fp, 0, SEEK_END);
-        /* Get posisiton of the file ptr, which is in turn file size */
+        /* Get posisiton of the file handle, which is in turn file size */
         fsize = ftell(fp);
-        /* if there is data */
-        if (fsize)
+
+        if (fsize) /* If there is data */
         {
-            /* move to start of the file */
+            /* Move back file handle to the start of file */
             fseek(fp, 0, SEEK_SET);
-            /* alloc memory to store the file data */
+            /* Alloc data pointer to read size */
             data = (unsigned char *)malloc(fsize);
+
+            /* Read data, assuming whole data will be read properlly in single call */
             if (fread(data, sizeof(unsigned char), fsize, fp) != fsize)
             {
                 printf("failed reading whole data.\n");
                 fclose(fp);
                 return 0;
             }
+
+            /* Tokenize input data with delimiter \n */
+            token = strtok((char *)data, "\n");
+            /* loop till token is not null */
+            while (token != NULL)
+            {
+                if (strlen(token) > 0) /* Minimum of single character */
+                {
+                    printf("%s\n", token);
+                }
+                token = strtok(NULL, "\n");
+            }
+            /* */
+            printf("Processed all data.\n");
         }
         else
             printf("Empty Dictionary.\n");
     }
     else
-        return 1;
+        return 0;
 
-    return 0;
+    return 1;
 }
