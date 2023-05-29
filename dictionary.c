8327eb92e78200771899c8ad3f2af5be7107f833
s-rajasekhar <raja_435@hotmail.com>
2023-05-29 22:22:51 +1000

    Store each token item in to the array of lists based on the ascii value of the first character.

--- dictionary.c
+++ dictionary.c
@@ -1,12 +1,58 @@
+/*
+    Assumes:
+    1. Each item/element in dictonary is delimited by a new line \n
+    2. Compiled using GCC, as this uses fnmatch
+*/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fnmatch.h>
 
 #define FILE_PATH "./words.txt" /* Input File, Can be read from Console, if time permit will add. Not that important. */
+#define ASCII_CHARS 128         /* Considers only first 128 ASCII characters */
+
+/* Structre to store each item from dictonary */
+typedef struct
+{
+    char *ptr; /* String pointer */
+    void *nxt; /* Next Node */
+} word_list;
+
+word_list *head[ASCII_CHARS];    /* Head Pointer */
+word_list *current[ASCII_CHARS]; /* Current Pointer */
+int total_words[ASCII_CHARS];    /* List count */
+
+/*
+    This function takes dictonary item as input
+*/
+void push(word_list *ptr)
+{
+    /* Find to which linked list this item need to be added */
+    /* Assumption, 128 ASCII characters only, hence on conversion of first char to int we will get the array item where we have to store this item */
+    unsigned int ascii = ptr->ptr[0];
+    /* If value is greater than valid ascii value ignore item */
+    if (ascii > ASCII_CHARS)
+        return;
+
+    /* If first item in the list, point to head */
+    if (!total_words[ascii])
+    {
+        head[ascii] = ptr;
+    }
+    else
+    {
+        /* Update previous nxt ptr */
+        current[ascii]->nxt = ptr;
+    }
+    current[ascii] = ptr;
+
+    /* Increment total items in the list */
+    total_words[ascii]++;
+}
 
 /* Return 1 on positive exit, else 0*/
-/*
-    Assumes, each item/element in dictonary is delimited by a new line \n
-*/
 int main()
 {
     unsigned char *data = NULL; /*Dictonary Data will be read into this pointer*/
@@ -46,7 +92,11 @@ int main()
             {
                 if (strlen(token) > 0) /* Minimum of single character */
                 {
-                    printf("%s\n", token);
+                    word_list *word = (word_list *)malloc(sizeof(word_list));
+                    word->ptr = token;
+                    word->nxt = NULL;
+                    /* Push new dictonary item */
+                    push(word);
                 }
                 token = strtok(NULL, "\n");
             }
